b71889dde0f5082dc208cbee84ef23e9bfa97d71
reubwn <reubennowell@gmail.com>
2017-06-14 16:45:32 +0100

    add bam

--- calculate_collinearity_breakpoints_from_BAM.pl
+++ calculate_collinearity_breakpoints_from_BAM.pl
@@ -6,6 +6,7 @@ use strict;
 use warnings;
 use Getopt::Long;
 use List::Util qw /sum/;
+use String::Random;
 use Sort::Naturally;
 
 my $usage = "
@@ -63,10 +64,15 @@ print STDOUT join (
 open (my $BED, $bed) or die "[ERROR] Cannot open $bed: $!\n";
 while (my $window = <$BED>) {
   #print STDERR "\r[INFO] Working on window \#$n: $_";$| = 1;
-  `printf "$window" > tmp.bed`;
+  #`printf "$window" > tmp.bed`;
+  my $rand = new String::Random;
+  my $tmp = $rand->randregex('\w\w\w\w\w\w\w').".bed";
+  open (my $TMP, ">$tmp");
+  print $TMP $window;
+  close $TMP;
   my ($total,$same,$insert,$insert_avg) = (0,0,0,0);
   my @insert_arr;
-  open(my $SAM, "samtools view -b $bam $region | bedtools intersect -sorted -g $genome -a stdin -b tmp.bed | samtools view - |");#`bedtools intersect -sorted -g $genome -a $bam -b <(printf "$_") | samtools view - | perl -lane 'if($F[6]eq"="){if($F[8]>500){$insert++};$same++;$total++}else{$total++}END{print "$total\t$same\t".($same/$total)."\t$insert\t".($insert/$total)}'`;
+  open(my $SAM, "samtools view -b $bam $region | bedtools intersect -sorted -g $genome -a stdin -b $tmp | samtools view - |");#`bedtools intersect -sorted -g $genome -a $bam -b <(printf "$_") | samtools view - | perl -lane 'if($F[6]eq"="){if($F[8]>500){$insert++};$same++;$total++}else{$total++}END{print "$total\t$same\t".($same/$total)."\t$insert\t".($insert/$total)}'`;
   while (<$SAM>) {
     my @F = split (/\s+/, $_);
     if ($F[6] eq "=") { ##mate on same scaffold
