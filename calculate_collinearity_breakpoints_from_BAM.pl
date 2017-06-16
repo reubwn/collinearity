a0b7a4f4a4429bf8bc725dea2f4ea93cce91144b
reubwn <reubennowell@gmail.com>
2017-06-16 12:15:32 +0100

    add stats

--- calculate_collinearity_breakpoints_from_BAM.pl
+++ calculate_collinearity_breakpoints_from_BAM.pl
@@ -6,13 +6,16 @@ use strict;
 use warnings;
 use Getopt::Long;
 use List::Util qw /sum/;
-use String::Random;
+use Statistics::Descriptive;
 use Sort::Naturally;
 
 my $usage = "
 SYNOPSIS
+  Calculates some statistics for sliding windows across a BAM file which can inform the presence of collinearity breakpoints
 
 OUTPUT
+  A file of stats per window, including total reads, average coverage, \%reads mapping to different contig, average insert,
+  \%reads with insert > specified threshold, \%reads split...
 
 OPTIONS
   -a|--bam    [FILE] : BAM file (required)
@@ -20,6 +23,7 @@ OPTIONS
   -b|--bed    [FILE] : windows in BED format (required)
   -g|--genome [FILE] : BEDtools style genome lengths file (required)
   -i|--insert [INT]  : insert size threshold; proportion of pairs with ISIZE >= this value reported (default: 500)
+  -d|--dryrun        : do a first pass to estimate 5% & 95% quantiles for ISIZE distribution, to inform -i parameter
   -o|--out           : outfile name (default: STDOUT)
   -h|--help          : print this message
 
@@ -27,9 +31,10 @@ USAGE
 
 \n";
 
-my ($bam,$bed,$genome,$help,$debug);
+my ($bam,$bed,$genome,$dryrun,$help,$debug);
 my $insert = 500;
 my $region = "";
+my @inserts_dryrun;
 my $n = 1;
 
 GetOptions (
@@ -37,7 +42,8 @@ GetOptions (
   'r|region:s' => \$region,
   'b|bed=s'  => \$bed,
   'g|genome=s' => \$genome,
-  'i|insert'  => \$insert,
+  'i|insert:i'  => \$insert,
+  'd|dryrun' => \$dryrun,
   'h|help'   => \$help,
   'd|debug'  => \$debug
 );
@@ -53,51 +59,65 @@ print STDOUT join (
   "\t",
   "CHROM\tSTART\tEND",#$window,
   "TOTAL",#$total,
-  "SAME",#$same,
-  "S:T",#($same/$total),
-  "INSERT",#$insert,
-  "I:T",#($insert/$total),
+  "DIFF",#$same,
+  "PROP_DIFF",#($same/$total),
+  "BIGINSERT",#$insert,
+  "PROP_BIGINSERT",#($insert/$total),
   "INSERT_AVG",#(sum(@insert_arr)/scalar(@insert_arr)),
   "\n"
 );
 
 open (my $BED, $bed) or die "[ERROR] Cannot open $bed: $!\n";
 while (my $window = <$BED>) {
-  #print STDERR "\r[INFO] Working on window \#$n: $_";$| = 1;
-  #`printf "$window" > tmp.bed`;
-  my $rand = new String::Random;
-  my $tmp = $rand->randregex('\w\w\w\w\w\w\w').".bed";
-  open (my $TMP, ">$tmp");
-  print $TMP $window;
+  chomp($window);
+  print STDERR "\r[INFO] Working on window \#$n: $window";$| = 1;
+
+  ## print single bed entry to tmp.bed
+  open (my $TMP, ">tmp.bed");
+  print $TMP "$window\n";
   close $TMP;
+
   my ($total,$same,$insert,$insert_avg) = (0,0,0,0);
   my @insert_arr;
-  open(my $SAM, "samtools view -b $bam $region | bedtools intersect -sorted -g $genome -a stdin -b $tmp | samtools view - |");#`bedtools intersect -sorted -g $genome -a $bam -b <(printf "$_") | samtools view - | perl -lane 'if($F[6]eq"="){if($F[8]>500){$insert++};$same++;$total++}else{$total++}END{print "$total\t$same\t".($same/$total)."\t$insert\t".($insert/$total)}'`;
+  open(my $SAM, "samtools view -f1792 -b $bam $region | bedtools intersect -sorted -g $genome -a stdin -b tmp.bed | samtools view - |");#`bedtools intersect -sorted -g $genome -a $bam -b <(printf "$_") | samtools view - | perl -lane 'if($F[6]eq"="){if($F[8]>500){$insert++};$same++;$total++}else{$total++}END{print "$total\t$same\t".($same/$total)."\t$insert\t".($insert/$total)}'`;
   while (<$SAM>) {
     my @F = split (/\s+/, $_);
-    if ($F[6] eq "=") { ##mate on same scaffold
-      $same++;
-      $total++;
-      $insert++ if ($F[8] > $insert);
-      push (@insert_arr, abs($F[8]));
+    if ($dryrun) {
+      if ($F[6] eq "=") { ##mate on same scaffold
+        push (@inserts_dryrun, abs($F[8]));
+      }
     } else {
-      $total++;
+      if ($F[6] eq "=") { ##mate on same scaffold
+        $same++;
+        $total++;
+        $insert++ if ($F[8] > $insert);
+        push (@insert_arr, abs($F[8]));
+      } else {
+        $total++;
+      }
     }
   }
   close $SAM;
-  chomp($window);
-  print STDOUT join (
-    "\t",
-    $window,
-    $total,
-    $same,
-    ($same/$total),
-    $insert,
-    ($insert/$total),
-    (sum(@insert_arr)/scalar(@insert_arr)),
-    "\n"
-  );
+  unless ($dryrun) {
+    print STDOUT join (
+      "\t",
+      $window,
+      $total,
+      ($total - $same),
+      (($total - $same)/$total),
+      $insert,
+      ($insert/$total),
+      (sum(@insert_arr)/scalar(@insert_arr)),
+      "\n"
+    );
+  }
 }
-
 close $BED;
+
+$stat = Statistics::Descriptive::Full->new();
+$stat->add_data(@inserts_dryrun);
+print STDERR "\n[INFO] ISIZE mean: ".$stat->mean()."\n";
+print STDERR "\n[INFO] ISIZE median: ".$stat->mean()."\n";
+print STDERR "\n[INFO] ISIZE 5\% and 95\%: ".$stat->percentile(5).", ".$stat->percentile(95)."\n";
+
 print STDERR "\n[INFO] Finished on ".`date`."\n";
