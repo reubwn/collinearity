f558c7f5fbe226f18917ad9135c52e39f88cd956
reubwn <reubennowell@gmail.com>
2018-03-06 19:46:18 +0000

    update

--- calculate_collinearity_breakpoints_from_GFF.pl
+++ calculate_collinearity_breakpoints_from_GFF.pl
@@ -211,7 +211,7 @@ foreach my $chrom (nsort keys %gff_hash) {
     my @blocks2 = @{$gff_hash{$arr[0]}} if (defined($arr[0]));
     my( $index1 ) = grep { $blocks1[$_] == $focal_block } 0..$#blocks1; ##get index of block in series of blocks on same chrom
     my( $index2 ) = grep { $blocks2[$_] == $focal_block } 0..$#blocks2; ##get index of HOMOLOGOUS block on HOMOLOGOUS chrom
-    print STDERR "$chrom, $index1\n" unless ($index2);
+    #print STDERR "$chrom, $index1\n" unless ($index2);
 
     if ( ("@blocks1" =~ "@blocks2") || ("@blocks1" =~ join (" ", reverse(@blocks2))) ) {
       $description = "B subset of A";
