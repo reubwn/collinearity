4aa0ee063ee4cfac9e416d877778750ed17892db
Reuben Nowell <rnowell@Reubens-MBP.domain_not_set.invalid>
2019-07-12 16:40:47 +0200

    update

--- aln_stats.pl
+++ aln_stats.pl
@@ -11,17 +11,18 @@ use Bio::SimpleAlign;
 
 my $usage = "
 SYNOPSIS
-  Calculates some simple alignment stats.
+  Calculates average percent identity (or divergence) for nucleotide alignments.
 
 OPTIONS
   -p|--path   [STRING] : path to files [required]
   -s|--suffix [STRING] : suffix of alignment files to glob from path (default 'fasta')
   -m|--format [STRING] : alignment format (default: 'fasta')
   -o|--out    [FILE]   : output file name (default: 'aln_stats.txt')
+  -d|--div             : print divergence rather than identity
   -h|--help            : this message
 \n";
 
-my ($path, $help);
+my ($path, $divergence, $help);
 my $suffix = "fasta";
 my $format = "fasta";
 my $outfile = "aln_stats.txt";
@@ -31,6 +32,7 @@ GetOptions (
   's|suffix:s'  => \$suffix,
   'm|format:s'  => \$format,
   'o|out:s'     => \$outfile,
+  'd|div'       => \$divergence,
   'h|help'      => \$help
 );
 
@@ -42,13 +44,21 @@ print STDERR "[INFO] Globbed ".scalar(@files)." '*.$suffix' files from $path\n";
 
 open (my $OUT_fh, ">$outfile") or die "$!\n";
 print STDERR "[INFO] Printing results to '$outfile'\n";
+print STDERR "[INFO] Printing divergence rather than identity\n" if ( $divergence );
 
 foreach my $file (nsort @files) {
   print $OUT_fh "$file\t";
   my $in = Bio::AlignIO->new( -file => $file, -format => $format );
   while ( my $aln_obj = $in->next_aln() ) {
     print $OUT_fh $aln_obj->num_sequences . "\t";
-    print $OUT_fh $aln_obj->percentage_identity . "\n";
+    if ( $divergence ) {
+      print $OUT_fh (100 - ($aln_obj->percentage_identity)) . "\n";
+    } else {
+      print $OUT_fh $aln_obj->percentage_identity . "\n";
+    }
   }
 }
 close $OUT_fh;
+print STDERR "[INFO] Finished on ".`date`."\n";
+
+__END__
